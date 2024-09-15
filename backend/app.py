03502cecba67a925a104eb2392f98fce33f7668c
mn39 <48751102+mn39@users.noreply.github.com>
2024-09-15 19:53:29 +0900

    fix2

--- backend/app.py
+++ backend/app.py
@@ -32,4 +32,4 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
