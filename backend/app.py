913ef2ecd891f48827d890856db29e8799ba8d35
mn39 <48751102+mn39@users.noreply.github.com>
2024-09-15 19:51:06 +0900

    fix..

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,25 @@ from flask import Flask, send_from_directory
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-
+from flask_cors import CORS
+import json
 load_dotenv()
 
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
 
+CORS(app)
+
 # MongoDB 연결
 client = MongoClient(os.environ.get('MONGODB_URI'))
-db = client['your_database']
+db = client['MN']
+collection = db['MN']
+
 
 @app.route('/api/data')
 def get_data():
-    data = db.collection.find_one()
-    return str(data)
+    data = collection.find_one()
+    return json.stringify(data)
 
 # 프론트엔드 정적 파일 서빙
 @app.route('/', defaults={'path': ''})
@@ -27,4 +32,4 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/data')
+      // fetch('http://127.0.0.1:5000/api/data')
+      .then((response) => {
+        console.log('response:', response);
+        if (!response.ok) {
+          throw new Error('API 요청 실패');
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('에러 발생:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {data ? <p>{data}</p> : <p>'데이터를 불러오는 중입니다...'</p>}
       </header>
     </div>
   );
